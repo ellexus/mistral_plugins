d1096dbed0ced992da52d2391777b68e10508b07
lorenaqendro <lorena@ellexus.com>
2016-04-13 17:32:30 +0100

    Changes after comments Includes changes to the test.sh that fixes #1476

--- output/mistral_out_mysql/src/log_receiver.c
+++ output/mistral_out_mysql/src/log_receiver.c
@@ -12,8 +12,6 @@
 #include "log_receiver.h"
 #include "log_to_mysql.h"
 
-const char *const dr_program_name = "MISTRAL MYSQL LOG";
-
 static unsigned ver = VERSION;          /* Supported version */
 static unsigned data_count = 0;         /* Number of data blocks received */
 static bool in_data = false;            /* True, if mistral is sending data */
@@ -22,7 +20,7 @@ static bool sup_ver = false;            /* True, if supported versions were rece
 static FILE *log_fs = NULL;
 
 /* Define how long to wait for reads before shutting down. */
-const struct timeval select_wait = {
+const struct timeval shutdown_delay = {
     .tv_sec = 1,
     .tv_usec = 0,
 };
@@ -49,17 +47,17 @@ static bool send_message_to_mistral(const char *message)
     struct timeval tv;
     int retval;
     FD_ZERO(&writeset);
-    FD_SET(LOG_OUTPUT, &writeset);
+    FD_SET(FD_OUTPUT, &writeset);
 
     /* Wait up to a default number of seconds. */
-    tv = select_wait;
+    tv = shutdown_delay;
 
     /* Keep trying to send data until all the message has been sent or an error
      * occurs.
      */
     while (message_len) {
         do {
-            retval = select(1 + LOG_OUTPUT, NULL, &writeset, NULL, &tv);
+            retval = select(1 + FD_OUTPUT, NULL, &writeset, NULL, &tv);
 
             if (retval == -1) {
                 char buf[256];
@@ -68,7 +66,7 @@ static bool send_message_to_mistral(const char *message)
                 return false;
             } else if (retval) {
                 ssize_t w_res =
-                    write(LOG_OUTPUT, message_start, message_len);
+                    write(FD_OUTPUT, message_start, message_len);
 
                 /* If we've successfully written some data, update the message
                  * pointer and length values
@@ -136,8 +134,6 @@ static bool send_shutdown_to_mistral()
  *
  * Function sends a start of data message
  *
- * Input:
- *   mistral_log     - Plugin configuration structure
  * Returns:
  *   False - On error
  *   True  - Otherwise
@@ -179,7 +175,8 @@ static bool send_version_to_mistral()
  */
 static int check_for_message(char *line)
 {
-    unsigned cont_count = data_count;
+    /* number of data blocks received */
+    unsigned block_count = data_count;
     int message = PLUGIN_MESSAGE_DATA_LINE;
     size_t line_len = strlen(line);
 
@@ -212,17 +209,17 @@ static int check_for_message(char *line)
             /* Strip any trailing newline for readability */
             STRIP_NEWLINE(line, line_len);
 
-            fprintf(log_fs, "Contract incomplete - saw [%s].\n", line);
-            return PLUGIN_MESSAGE_SHUTDOWN;
+            fprintf(log_fs, "Data block incomplete - saw [%s].\nLog data might be corrupted.\n", line);
+            return PLUGIN_DATA_ERR;
         }
         }
     } else {
-        /* Not in a contract so only control messages are valid */
+        /* Not in a data block so only control messages are valid */
         if (message == PLUGIN_MESSAGE_DATA_LINE && *line != '\n') {
             /* strip any trailing newline for readability */
             STRIP_NEWLINE(line, line_len);
 
-            fprintf(log_fs, "Invalid data: [%s].\n", line);
+            fprintf(log_fs, "Invalid data: [%s]. Expected a control message.\n", line);
             return PLUGIN_DATA_ERR;
         }
     }
@@ -233,7 +230,7 @@ static int check_for_message(char *line)
         /* strip any trailing newline for readability */
         STRIP_NEWLINE(line, line_len);
 
-        fprintf(log_fs, "Invalid data: [%s].\n", line);
+        fprintf(log_fs, "Invalid data: [%s]. Expected log message or PLUGIN_MESSAGE_END.\n", line);
         return PLUGIN_DATA_ERR;
     }
 
@@ -247,7 +244,7 @@ static int check_for_message(char *line)
     case PLUGIN_MESSAGE_USED_VERSION:
     case PLUGIN_MESSAGE_INTERVAL: {
         /* We should never receive this message */
-        fprintf(log_fs, "Invalid data: [%s].\n", line);
+        fprintf(log_fs, "Invalid data: [%s]. Don't expect to receive this message.\n", line);
         return PLUGIN_DATA_ERR;
         break;
     }
@@ -285,36 +282,36 @@ static int check_for_message(char *line)
 
         p = line + mistral_log_msg_len[PLUGIN_MESSAGE_DATA_START];
 
-        cont_count = strtoul(p, &end, 10);
+        block_count = strtoul(p, &end, 10);
 
-        if (cont_count <= 0 || cont_count > UINT_MAX || *end != PLUGIN_MESSAGE_SEP_C) {
+        if (block_count <= 0 || block_count > UINT_MAX || *end != PLUGIN_MESSAGE_SEP_C) {
             fprintf(log_fs, "Invalid contract count: [%s].\n", line);
             return PLUGIN_DATA_ERR;
         }
         in_data = true;
-        data_count = cont_count;
+        data_count = block_count;
         break;
     }
     case PLUGIN_MESSAGE_DATA_END: {
         /* Check if the contract number matches the last version we sent */
-        unsigned  end_cont_count = 0;
+        unsigned  end_block_count = 0;
         char *p = NULL;
         char *end = NULL;
 
         p = line + mistral_log_msg_len[PLUGIN_MESSAGE_DATA_END];
 
-        end_cont_count = strtoul(p, &end, 10);
+        end_block_count = strtoul(p, &end, 10);
 
-        if (end_cont_count <= 0 || end_cont_count > UINT_MAX ||
+        if (end_block_count <= 0 || end_block_count > UINT_MAX ||
                 *end != PLUGIN_MESSAGE_SEP_C) {
             fprintf(log_fs, "Invalid contract count: [%s].\n", line);
             return PLUGIN_DATA_ERR;
         }
 
-        if (data_count != end_cont_count ) {
+        if (data_count != end_block_count ) {
             fprintf(log_fs,
                     "Contract instance count mismatch. Saw [%d], expected [%d].\nLog data might be corrupted.\n",
-                    end_cont_count, data_count);
+                    end_block_count, data_count);
             return PLUGIN_DATA_ERR;
         }
         in_data = false;
@@ -369,6 +366,12 @@ char **str_split(const char *s, int sep, size_t *sep_count)
     return result;
 }
 
+/*
+ * parse_log_entry()
+ * 
+ * Parse the line received. The line structure is <scope>:<type>:log_message
+ * log_message contains comma separated strings.
+ */
 bool parse_log_entry(char *line)
 {
     bool ret = true;
@@ -385,7 +388,7 @@ bool parse_log_entry(char *line)
         ret = false;
         goto err_sc_nr; 
     }
-    //todo check if the arrays are the right size
+
     mistral_log_entry_s log_entry = {0};
     mistral_log_msg_s msg = {0};
     log_entry.log_msg = &msg;
@@ -426,31 +429,31 @@ err_comma_nr:
     return ret;
 }
 
-/* Function that reads from stdin.
- * Reads first the messages from the monitor and the contract.
- * Saves the contract in a contract_update structure.
+/* 
+ * read_data_from_mistral()
+ *
+ * Function that reads from FD_INPUT file descriptor.
+ * Calls parse_log_entry() to parse the received message.
  */
 static bool read_data_from_mistral()
 {
     int retval;
     struct timeval tv;
 
-
-
     /* waiting time set to 0 */
-    tv = select_wait;
+    tv = shutdown_delay;
     fd_set readset;
 
     /* check stdin (fd 0) to see when it has input. */
     FD_ZERO(&readset);
-    FD_SET(LOG_INPUT, &readset);
+    FD_SET(FD_INPUT, &readset);
     char *line = NULL;
     size_t line_length = 0;
 
     bool ret = false;
 
     do {
-        retval = select(1 + LOG_INPUT, &readset, NULL, NULL, &tv);
+        retval = select(1 + FD_INPUT, &readset, NULL, NULL, &tv);
 
         if (retval == -1) {
             char buf[256];
@@ -570,7 +573,7 @@ int main(int argc, char **argv)
     }
 
     if (config_file == NULL) {
-        fprintf(stderr, "Missing option -c.\n");
+        fprintf(stderr, "Missing option -c. Impossible to connect to the database without specifying a configuration file.\n");
         exit(EXIT_FAILURE);
     }
 
--- output/mistral_out_mysql/test/test.sh
+++ output/mistral_out_mysql/test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Test funcionality of the mysql log plugin.
+# Test funcionality of the mistral output mysql plugin.
 
 # Create the config file to be read by mistral
 func_create_plugin_conf() {
@@ -37,14 +37,11 @@ func_create_plugin_conf() {
 }
 
 func_run_config_test() {
-    if [ "${OUTPUT_OPTS-_UnSeT_}" != "_UnSeT_" ]; then
+    if [ "${OUTPUT_OPTS:-_UnSeT_}" != "_UnSeT_" ]; then
         OUTPUT_OPTS="${OUTPUT_OPTS} --output ${PLUGIN_CONFIG}_out"
     fi
 
     eval "export ${BRANDING}_PLUGIN_CONFIG=\$PLUGIN_CONFIG"
-    # Use the following to get a file with logs to compare with the ones written in the DB
-    #func_create_plugin_conf OUTPUT INT${OUT_INT} ${OUTPUT_OPTS} END
-    #${PWD}/mistral_test/mistral_trunk_x86_64/mistral ./file_io -l 10 -b 10 -o 100 ./output_file_io
 
     func_create_plugin_conf MYSQL INT${MYSQL_INT} ${OUTPUT_OPTS} END
 
@@ -56,19 +53,17 @@ func_run_config_test() {
         exit
     fi
 
-    #${PWD}/mistral_trunk_x86_64/mistral ./file_io -l 10 -b 10 -o 100 ./output_file_io
     # Infinite loop of file_io calls
-    ${PWD}/mistral_test/mistral_trunk_x86_64/mistral ${PWD}/test_always_on.sh
+    ${PWD}/mistral_test/mistral ${PWD}/test_always_on.sh
 
 }
 
-# Set up local and global contracts, only local contract information should be
-# sent to the plugin.
 export RESULTS_DIR=$HOME
 export BRANDING=MISTRAL
 
-CUSTOM_LABEL=$(echo TEST_SQL_$$_$HOSTNAME_$(date +"%m_%d_%yT%H_%M_%S") | sed 's/\./\_/g' )
+CUSTOM_LABEL=$(echo TEST_SQL_$$_$HOSTNAME_$(date +"%m_%d_%yT%H_%M_%S"))
 
+# Set up local/global contracts
 export CONTRACT_MONITOR_GLOBAL=$RESULTS_DIR/global_monitor_contract
 echo "monitortimeframe,50ms" > "$CONTRACT_MONITOR_GLOBAL"
 echo "$CUSTOM_LABEL,/home,read,bandwidth,1B" >> "$CONTRACT_MONITOR_GLOBAL"
@@ -82,32 +77,16 @@ eval "export ${BRANDING}_LOG_MONITOR_GLOBAL=\$RESULTS_DIR/global_monitor.log"
 #eval "export ${BRANDING}_LOG_THROTTLE_GLOBAL=\$RESULTS_DIR/global_throttle.log"
 
 
+# the licence should be where mistral is extracted
+export ${BRANDING}_RLM_LICENSE="./mistral_test"
 
-eval "export ${BRANDING}_RLM_LICENSE=/home/lorenaq/Desktop/ellexus_repo/breeze-repo/"
-
-OUT_INT=300
+# Value of INTERVAL in the plugin configuration file
 MYSQL_INT=1
 
 PLUGIN_CONFIG=${RESULTS_DIR}/plugin.conf
 export TEST_PATH
 export PLUGIN_EXIT=no
 OUTPUT_OPTS=
-OUTPUT_DIR=$RESULTS_DIR/log_plug
+OUTPUT_DIR=$RESULTS_DIR/mysql_out_plugin
 
 func_run_config_test
-
-line=$(head -n 1  ${PWD}/query_res.txt)
-num_logs_label=$(grep -c "TEST_" ${PLUGIN_CONFIG}_out)
-
-#This comparison is valid only if both output and mysql_output are ran. 
-#This way there is a way to compare the number oflogs from output plugin 
-#and the ones written in the DB. 
-if [ $line -ne $num_logs_label ]; then
-    echo $line
-    echo $num_logs_label
-    echo FAILURE
-else
-    echo SUCCESS
-fi
-
-rm ${PWD}/query_res.txt
