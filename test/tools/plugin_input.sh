879658ddec7234a93379aa36f086fc3025931512
Paul Geldart <paul@ellexus.com>
2016-07-14 14:56:38 +0100

    Update calculated times to cover more scenareos
    Make sure all dates are calculated relative to the same reference time.
    In addition calculate the UTC version of each of these times.
    
    Update the InfluxDB test case to use the new values.  I haven't tested
    but I believe the test will currently fail if run between 00:00 and 01:00

--- test/tools/plugin_input.sh
+++ test/tools/plugin_input.sh
@@ -37,41 +37,173 @@ elif [ ! -w "$output_dir" ]; then
     exit 4
 fi
 
+refdate=$(date +"%a %b %e %T %Z %Y")
 # Set up some substitution variables
-date1=$(date +%F)
-date2=$(date --date="yesterday" +%F)
-date3=$(date --date="2 days ago" +%F)
-date4=$(date --date="7 days ago" +%F)
-date5=$(date --date="8 days ago" +%F)
-date6=$(date --date="30 days ago" +%F)
-date7=$(date --date="45 days ago" +%F)
-date8=$(date --date="60 days ago" +%F)
+datetime1=$(date --date="$refdate" +"%F %T")
+datetime2=$(date --date="$refdate -1 day" +"%F %T")
+datetime3=$(date --date="$refdate -2 days" +"%F %T")
+datetime4=$(date --date="$refdate -7 days" +"%F %T")
+datetime5=$(date --date="$refdate -8 days" +"%F %T")
+datetime6=$(date --date="$refdate -30 days" +"%F %T")
+datetime7=$(date --date="$refdate -45 days" +"%F %T")
+datetime8=$(date --date="$refdate -60 days" +"%F %T")
+dateday1=${datetime1% *}
+dateday2=${datetime2% *}
+dateday3=${datetime3% *}
+dateday4=${datetime4% *}
+dateday5=${datetime5% *}
+dateday6=${datetime6% *}
+dateday7=${datetime7% *}
+dateday8=${datetime8% *}
+datetime1=${datetime1#* }
+datetime2=${datetime2#* }
+datetime3=${datetime3#* }
+datetime4=${datetime4#* }
+datetime5=${datetime5#* }
+datetime6=${datetime6#* }
+datetime7=${datetime7#* }
+datetime8=${datetime8#* }
 
-time1=$(date +%R)
-time2=$(date --date="1 minute ago" +%R)
-time3=$(date --date="2 minutes ago" +%R)
-time4=$(date --date="3 minutes ago" +%R)
-time5=$(date --date="4 minutes ago" +%R)
-time6=$(date --date="5 minutes ago" +%R)
-time7=$(date --date="15 minutes ago" +%R)
-time8=$(date --date="60 minutes ago" +%R)
+utcdatetime1=$(date -u --date="$refdate" +"%F %T")
+utcdatetime2=$(date -u --date="$refdate -1 day" +"%F %T")
+utcdatetime3=$(date -u --date="$refdate -2 days" +"%F %T")
+utcdatetime4=$(date -u --date="$refdate -7 days" +"%F %T")
+utcdatetime5=$(date -u --date="$refdate -8 days" +"%F %T")
+utcdatetime6=$(date -u --date="$refdate -30 days" +"%F %T")
+utcdatetime7=$(date -u --date="$refdate -45 days" +"%F %T")
+utcdatetime8=$(date -u --date="$refdate -60 days" +"%F %T")
+utcdateday1=${utcdatetime1% *}
+utcdateday2=${utcdatetime2% *}
+utcdateday3=${utcdatetime3% *}
+utcdateday4=${utcdatetime4% *}
+utcdateday5=${utcdatetime5% *}
+utcdateday6=${utcdatetime6% *}
+utcdateday7=${utcdatetime7% *}
+utcdateday8=${utcdatetime8% *}
+utcdatetime1=${utcdatetime1#* }
+utcdatetime2=${utcdatetime2#* }
+utcdatetime3=${utcdatetime3#* }
+utcdatetime4=${utcdatetime4#* }
+utcdatetime5=${utcdatetime5#* }
+utcdatetime6=${utcdatetime6#* }
+utcdatetime7=${utcdatetime7#* }
+utcdatetime8=${utcdatetime8#* }
 
-echo "s/DATE1/$date1/g" > "$output"
-echo "s/DATE2/$date2/g" >> "$output"
-echo "s/DATE3/$date3/g" >> "$output"
-echo "s/DATE4/$date4/g" >> "$output"
-echo "s/DATE5/$date5/g" >> "$output"
-echo "s/DATE6/$date6/g" >> "$output"
-echo "s/DATE7/$date7/g" >> "$output"
-echo "s/DATE8/$date8/g" >> "$output"
-echo "s/TIME1/$time1/g" >> "$output"
-echo "s/TIME2/$time2/g" >> "$output"
-echo "s/TIME3/$time3/g" >> "$output"
-echo "s/TIME4/$time4/g" >> "$output"
-echo "s/TIME5/$time5/g" >> "$output"
-echo "s/TIME6/$time6/g" >> "$output"
-echo "s/TIME7/$time7/g" >> "$output"
-echo "s/TIME8/$time8/g" >> "$output"
+
+time1=$(date --date="$refdate" +"%F %T")
+time2=$(date --date="$refdate -1 minute" +"%F %T")
+time3=$(date --date="$refdate -2 minutes" +"%F %T")
+time4=$(date --date="$refdate -3 minutes" +"%F %T")
+time5=$(date --date="$refdate -4 minutes" +"%F %T")
+time6=$(date --date="$refdate -5 minutes" +"%F %T")
+time7=$(date --date="$refdate -15 minutes" +"%F %T")
+time8=$(date --date="$refdate -60 minutes" +"%F %T")
+tsday1=${time1% *}
+tsday2=${time2% *}
+tsday3=${time3% *}
+tsday4=${time4% *}
+tsday5=${time5% *}
+tsday6=${time6% *}
+tsday7=${time7% *}
+tsday8=${time8% *}
+tstime1=${time1#* }
+tstime2=${time2#* }
+tstime3=${time3#* }
+tstime4=${time4#* }
+tstime5=${time5#* }
+tstime6=${time6#* }
+tstime7=${time7#* }
+tstime8=${time8#* }
+
+utctime1=$(date -u --date="$refdate" +"%F %T")
+utctime2=$(date -u --date="$refdate -1 minute" +"%F %T")
+utctime3=$(date -u --date="$refdate -2 minutes" +"%F %T")
+utctime4=$(date -u --date="$refdate -3 minutes" +"%F %T")
+utctime5=$(date -u --date="$refdate -4 minutes" +"%F %T")
+utctime6=$(date -u --date="$refdate -5 minutes" +"%F %T")
+utctime7=$(date -u --date="$refdate -15 minutes" +"%F %T")
+utctime8=$(date -u --date="$refdate -60 minutes" +"%F %T")
+utctsday1=${utctime1% *}
+utctsday2=${utctime2% *}
+utctsday3=${utctime3% *}
+utctsday4=${utctime4% *}
+utctsday5=${utctime5% *}
+utctsday6=${utctime6% *}
+utctsday7=${utctime7% *}
+utctsday8=${utctime8% *}
+utctstime1=${utctime1#* }
+utctstime2=${utctime2#* }
+utctstime3=${utctime3#* }
+utctstime4=${utctime4#* }
+utctstime5=${utctime5#* }
+utctstime6=${utctime6#* }
+utctstime7=${utctime7#* }
+utctstime8=${utctime8#* }
+
+echo "s/UTCDATE1/$utcdateday1/g" > "$output"
+echo "s/UTCDATE2/$utcdateday2/g" >> "$output"
+echo "s/UTCDATE3/$utcdateday3/g" >> "$output"
+echo "s/UTCDATE4/$utcdateday4/g" >> "$output"
+echo "s/UTCDATE5/$utcdateday5/g" >> "$output"
+echo "s/UTCDATE6/$utcdateday6/g" >> "$output"
+echo "s/UTCDATE7/$utcdateday7/g" >> "$output"
+echo "s/UTCDATE8/$utcdateday8/g" >> "$output"
+echo "s/UTCTIME1/$utcdatetime1/g" >> "$output"
+echo "s/UTCTIME2/$utcdatetime2/g" >> "$output"
+echo "s/UTCTIME3/$utcdatetime3/g" >> "$output"
+echo "s/UTCTIME4/$utcdatetime4/g" >> "$output"
+echo "s/UTCTIME5/$utcdatetime5/g" >> "$output"
+echo "s/UTCTIME6/$utcdatetime6/g" >> "$output"
+echo "s/UTCTIME7/$utcdatetime7/g" >> "$output"
+echo "s/UTCTIME8/$utcdatetime8/g" >> "$output"
+echo "s/UTCTSDATE1/$utctsday1/g" >> "$output"
+echo "s/UTCTSDATE2/$utctsday2/g" >> "$output"
+echo "s/UTCTSDATE3/$utctsday3/g" >> "$output"
+echo "s/UTCTSDATE4/$utctsday4/g" >> "$output"
+echo "s/UTCTSDATE5/$utctsday5/g" >> "$output"
+echo "s/UTCTSDATE6/$utctsday6/g" >> "$output"
+echo "s/UTCTSDATE7/$utctsday7/g" >> "$output"
+echo "s/UTCTSDATE8/$utctsday8/g" >> "$output"
+echo "s/UTCTSTIME1/$utctstime1/g" >> "$output"
+echo "s/UTCTSTIME2/$utctstime2/g" >> "$output"
+echo "s/UTCTSTIME3/$utctstime3/g" >> "$output"
+echo "s/UTCTSTIME4/$utctstime4/g" >> "$output"
+echo "s/UTCTSTIME5/$utctstime5/g" >> "$output"
+echo "s/UTCTSTIME6/$utctstime6/g" >> "$output"
+echo "s/UTCTSTIME7/$utctstime7/g" >> "$output"
+echo "s/UTCTSTIME8/$utctstime8/g" >> "$output"
+echo "s/TSDATE1/$tsday1/g" >> "$output"
+echo "s/TSDATE2/$tsday2/g" >> "$output"
+echo "s/TSDATE3/$tsday3/g" >> "$output"
+echo "s/TSDATE4/$tsday4/g" >> "$output"
+echo "s/TSDATE5/$tsday5/g" >> "$output"
+echo "s/TSDATE6/$tsday6/g" >> "$output"
+echo "s/TSDATE7/$tsday7/g" >> "$output"
+echo "s/TSDATE8/$tsday8/g" >> "$output"
+echo "s/TSTIME1/$tstime1/g" >> "$output"
+echo "s/TSTIME2/$tstime2/g" >> "$output"
+echo "s/TSTIME3/$tstime3/g" >> "$output"
+echo "s/TSTIME4/$tstime4/g" >> "$output"
+echo "s/TSTIME5/$tstime5/g" >> "$output"
+echo "s/TSTIME6/$tstime6/g" >> "$output"
+echo "s/TSTIME7/$tstime7/g" >> "$output"
+echo "s/TSTIME8/$tstime8/g" >> "$output"
+echo "s/DATE1/$dateday1/g" >> "$output"
+echo "s/DATE2/$dateday2/g" >> "$output"
+echo "s/DATE3/$dateday3/g" >> "$output"
+echo "s/DATE4/$dateday4/g" >> "$output"
+echo "s/DATE5/$dateday5/g" >> "$output"
+echo "s/DATE6/$dateday6/g" >> "$output"
+echo "s/DATE7/$dateday7/g" >> "$output"
+echo "s/DATE8/$dateday8/g" >> "$output"
+echo "s/TIME1/$datetime1/g" >> "$output"
+echo "s/TIME2/$datetime2/g" >> "$output"
+echo "s/TIME3/$datetime3/g" >> "$output"
+echo "s/TIME4/$datetime4/g" >> "$output"
+echo "s/TIME5/$datetime5/g" >> "$output"
+echo "s/TIME6/$datetime6/g" >> "$output"
+echo "s/TIME7/$datetime7/g" >> "$output"
+echo "s/TIME8/$datetime8/g" >> "$output"
 
 # The IFS='' prevents leading and trailing whitespace from being trimed, and the
 # -n test deals with a missing trailing newline on the last line of input
