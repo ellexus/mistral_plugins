8e4dc9a6717a4ccb626986bed0197460604f1b28
Nick Barnes <nick@ellexus.com>
2020-05-21 11:17:30 +0100

    Update plugin version.

--- common/plugin_control.h
+++ common/plugin_control.h
@@ -19,7 +19,7 @@ size_t mistral_call_type_len[CALL_TYPE_MAX] = {
 };
 
 /* Version of the API used by the plug-in */
-#define MISTRAL_API_VERSION 5
+#define MISTRAL_API_VERSION 6
 
 /* Define the number of fields in the plugin message string */
 #define PLUGIN_MESSAGE_FIELDS 3
